a8d337e882a6f266a6057d0179c6c7095aa1676a
Jinser Kafka <aimer@purejs.icu>
2024-08-22 15:02:20 +0800

    manually update to latest

--- versions/latest.nix
+++ versions/latest.nix
@@ -1,4 +1,4 @@
 {
-  cliHash = "sha256-qwXnt7CA76A+LTJDuNpYmjEN0lbTELQK+MpKT7Siiw8=";
-  coreHash = "sha256-/GEo6eLmxaOnJhAjuC3ldqu+RQyE6GpQojKI8mgS8OA=";
+  cliHash = "sha256-I3wRU8WzmmzLJJB/LPkpTqR/uEUBrhEb76MD1nduOzc=";
+  coreHash = "sha256-wYA0vqY8SVAtSvV8i1OCJu+IvTjt2HEiz+dIyQORvik=";
 }
