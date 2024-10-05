b098a92f19ef7cc19b91e2889cd41db057913df7
Jinser Kafka <aimer@purejs.icu>
2024-10-05 15:17:08 +0800

    manually update to latest again

--- versions/latest.nix
+++ versions/latest.nix
@@ -1,4 +1,4 @@
 {
-  cliHash = "sha256-I3wRU8WzmmzLJJB/LPkpTqR/uEUBrhEb76MD1nduOzc=";
-  coreHash = "sha256-wYA0vqY8SVAtSvV8i1OCJu+IvTjt2HEiz+dIyQORvik=";
+  cliHash = "sha256-42ja+ATvdVqtSufm+j1SHQ5l24GlstVSDAErmWqyCCI=";
+  coreHash = "sha256-UnJCGYcHJNWPqgQcgJe7p4qGx+okRYtoqJxBc7TxzmQ=";
 }
