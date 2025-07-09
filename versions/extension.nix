ac5e29de6b77c6e68a959d896777cddb24fde462
GitHub Action <action@github.com>
2025-07-09 03:23:46 +0000

    versions: update

--- versions/extension.nix
+++ versions/extension.nix
@@ -1,5 +1,6 @@
 {
-  latest = "Mw1NH1lkljFD05fIuSiYS4sIKDjOiqGx++fArCVBW28=";
+  latest = "DjRzvzigOYIzlf/S+PzSgx/I5GfVxwXIFbTvZ1En2fM=";
+  "0_1_2025070801" = "DjRzvzigOYIzlf/S+PzSgx/I5GfVxwXIFbTvZ1En2fM=";
   "0_1_2025070701" = "Mw1NH1lkljFD05fIuSiYS4sIKDjOiqGx++fArCVBW28=";
   "0_1_2025070404" = "uEJa34Eiqm4ZnLY9axxOQmE6hReOa3KIKdJSx4DBz3A=";
   "0_1_2025070403" = "fkxmX85vz3Obxki6ISahWrz4h+RxWufC3pkVAapHsBI=";
