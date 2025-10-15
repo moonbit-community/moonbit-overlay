4133a9f93de7f56b07046992f54cff2959b70f81
Selena Zhou <selena_zhou2006@outlook.com>
2025-10-15 12:20:01 +0800

    rename cli derivation

--- lib/cli.nix
+++ lib/cli.nix
@@ -14,7 +14,7 @@
   ...
 }:
 stdenv.mkDerivation {
-  pname = "moonbit-cli";
+  pname = "moonbit-toolchains";
   inherit version;
 
   src = fetchurl {
@@ -34,25 +34,25 @@ stdenv.mkDerivation {
   ];
 
   installPhase = ''
-      runHook preInstall
+    runHook preInstall
 
-      mkdir -p $out
-      cp -a ./* $out/
-      chmod +x $out/bin/*
-      chmod +x $out/bin/internal/tcc
+    mkdir -p $out
+    cp -a ./* $out/
+    chmod +x $out/bin/*
+    chmod +x $out/bin/internal/tcc
 
-    ''
-    + lib.optionalString (version != "latest") ''
-      cp ${moon-patched}/bin/moon $out/bin/moon
-      cp ${moon-patched}/bin/moonrun $out/bin/moonrun
+  ''
+  + lib.optionalString (version != "latest") ''
+    cp ${moon-patched}/bin/moon $out/bin/moon
+    cp ${moon-patched}/bin/moonrun $out/bin/moonrun
 
-      rm $out/bin/internal/tcc
-      ln -sf ${tinycc.out}/bin/tcc $out/bin/internal/tcc
-    ''
-    + ''
+    rm $out/bin/internal/tcc
+    ln -sf ${tinycc.out}/bin/tcc $out/bin/internal/tcc
+  ''
+  + ''
 
-      runHook postInstall
-    '';
+    runHook postInstall
+  '';
 
   meta = {
     homepage = "moonbitlang.com";
